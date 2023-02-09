ec44f3f82f6c835ee52d4870bf19c207797995c8
Łukasz Wroński <lukaszwronski98@gmail.com>
2023-02-09 15:16:26 +0100

    Add clang to scala-cli docker image

--- modules/integration/docker/src/test/scala/scala/cli/integration/RunDockerTests.scala
+++ modules/integration/docker/src/test/scala/scala/cli/integration/RunDockerTests.scala
@@ -10,6 +10,8 @@ class RunDockerTests extends munit.FunSuite {
   lazy val imageName = Option(System.getenv("SCALA_CLI_IMAGE")).getOrElse {
     sys.error("SCALA_CLI_IMAGE not set")
   }
+  lazy val termOpt = if (System.console() == null) Nil else Seq("-t")
+  lazy val ciOpt   = Option(System.getenv("CI")).map(v => Seq("-e", s"CI=$v")).getOrElse(Nil)
 
   test("run simple app in in docker") {
     val fileName = "simple.sc"
@@ -21,8 +23,6 @@ class RunDockerTests extends munit.FunSuite {
            |""".stripMargin
     )
     inputs.fromRoot { root =>
-      val termOpt   = if (System.console() == null) Nil else Seq("-t")
-      val ciOpt     = Option(System.getenv("CI")).map(v => Seq("-e", s"CI=$v")).getOrElse(Nil)
       val rawOutput = new ByteArrayOutputStream
       val cmd = Seq[os.Shellable](
         // format: off
@@ -40,6 +40,23 @@ class RunDockerTests extends munit.FunSuite {
       )
       val output = new String(rawOutput.toByteArray, Charset.defaultCharset())
       expect(output.linesIterator.toVector.last == message)
+    }
+  }
+
+  test("package simple app with native in docker") {
+    val fileName = "simple.sc"
+    val inputs = TestInputs(
+      os.rel / fileName -> """println("Hello")"""
+    )
+    inputs.fromRoot { root =>
+      val cmdPackage = Seq[os.Shellable](
+        // format: off
+        "docker", "run", "--rm", termOpt, "-v", s"$root:/data", "-w", "/data", ciOpt,
+        imageName, "package", "--native", fileName, "-o", "Hello"
+        // format: on
+      )
+      val procPackage = os.proc(cmdPackage).call(cwd = root)
+      expect(procPackage.exitCode == 0)
     }
   }
 }
